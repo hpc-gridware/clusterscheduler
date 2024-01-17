c090f62f529f947adb18f6dff2c50c344cb99f10
Joachim Gabler <jgabler@hpc-gridware.com>
2024-01-17 09:58:21 +0100

    EH: OGE-40 add documentation comments from old CULL files to the JSON files // RN_Type

--- source/libs/sgeobj/cull/sge_range_RN_L.h
+++ source/libs/sgeobj/cull/sge_range_RN_L.h
@@ -40,18 +40,61 @@ extern "C" {
 #endif
 
 /**
-* @brief @todo add summary
+* @brief Range Element
 *
-* @todo add description
+* 
+* Object holding values which define an id range
+* (e.g. 1-9:2 => 1, 3, 5, 7, 9).
+* Lists of such objects are held within job objects
+* (JB_Type) to hold job array task ids.
+* Several functions may be used to access/modify/delete range
+* elements and range lists. You may find them in the 'SEE ALSO'
+* section below. It is highly advised to use these access functions
+* because they assure and require a defined structure of
+* elements and lists.
+* 
+* Range elements and lists stored in other objects fullfill
+* following conditions:
+* 
+*    - min <= max
+*    - step >= 1
+*    - real range elements (e.g. 1-9:2 instead of 1-10:2)
+*    - min-ids within range elements part of the same
+*      list are in ascending order: min_id(n) < min_id(n+1)
+*      (e.g. NOT 11-20:1; 1-9:2)
+*    - ids within range elements part of the same
+*      list are non-overlapping: max_id(n) < min_id(n+1)
+*      (e.g. 1-9:2; 11-20:1; 25-28:3)
+* 
+* @see gdi/range/range_list_calculate_union_set()
+* @see gdi/range/range_list_calculate_difference_set()
+* @see gdi/range/range_list_calculate_intersection_set() 
+* @see gdi/range/range_list_compress()
+* @see gdi/range/range_list_get_first_id()
+* @see gdi/range/range_list_get_last_id()
+* @see gdi/range/range_list_get_number_of_ids()
+* @see gdi/range/range_list_initialize()
+* @see gdi/range/range_list_insert_id()
+* @see gdi/range/range_list_is_id_within()
+* @see gdi/range/range_list_move_first_n_ids()
+* @see gdi/range/range_list_print_to_string()
+* @see gdi/range/range_list_remove_id()
+* @see gdi/range/range_correct_end()
+* @see gdi/range/range_get_all_ids()
+* @see gdi/range/range_get_number_of_ids()
+* @see gdi/range/range_is_overlapping()
+* @see gdi/range/range_is_id_within()
+* @see gdi/range/range_set_all_ids()
+* @see gdi/range/range_sort_uniq_compress()
 *
-*    SGE_ULONG(RN_min) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(RN_min) - Lower Bound
+*    minimum or start value of an id range (e.g. 1)
 *
-*    SGE_ULONG(RN_max) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(RN_max) - Upper Bound
+*    maximum or end value of an id range (e.g. 9)
 *
-*    SGE_ULONG(RN_step) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(RN_step) - Step Size
+*    stepsize (e.g. 2)
 *
 */
 
