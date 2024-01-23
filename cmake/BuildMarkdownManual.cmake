3f2aadac3a510a267b4a451a2a127a655ac83cb2
Ernst Bablick <ebablick@hpc-gridware.com>
2024-01-24 00:15:59 +0100

    TA: OGE-110 enhance build environment to create documentation on top of common documentation

--- cmake/BuildMarkdownManual.cmake
+++ cmake/BuildMarkdownManual.cmake
@@ -1,13 +1,14 @@
 function(build_markdown_manual file title)
-    string(TIMESTAMP DATETIME "%Y-%m-%d")
-    add_custom_command(
-            OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.pdf
-            DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/*.md
-                    ${CMAKE_CURRENT_SOURCE_DIR}/../titlepage.md
-                    ${CMAKE_CURRENT_SOURCE_DIR}/../copyright.md
-                    ${CMAKE_CURRENT_SOURCE_DIR}/../typographic_conventions.md
-                    ${CMAKE_CURRENT_SOURCE_DIR}/../head.tex
-            COMMAND ${CMAKE_SOURCE_DIR}/doc/scripts/pandoc_manual.sh ${CMAKE_CURRENT_SOURCE_DIR}
-                    ${CMAKE_CURRENT_BINARY_DIR} ${file} ${title} ${CMAKE_PROJECT_VERSION} ${DATETIME}
-    )
+   string(TIMESTAMP DATETIME "%Y-%m-%d")
+   add_custom_command(
+         OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.pdf
+         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/*.md
+         ${CMAKE_SOURCE_DIR}/../gridengine/doc/markdown/manual/titlepage.md
+         ${CMAKE_SOURCE_DIR}/../gridengine/doc/markdown/manual/copyright.md
+         ${CMAKE_SOURCE_DIR}/../gridengine/doc/markdown/manual/typographic_conventions.md
+         ${CMAKE_SOURCE_DIR}/../gridengine/doc/markdown/manual/head.tex
+         ${CMAKE_SOURCE_DIR}/../gridengine/doc/scripts/pandoc_manual.sh
+         COMMAND ${CMAKE_SOURCE_DIR}/doc/scripts/pandoc_manual.sh ${CMAKE_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}
+         ${CMAKE_CURRENT_BINARY_DIR} ${file} ${title} ${CMAKE_PROJECT_VERSION} ${DATETIME}
+   )
 endfunction()
--- doc/markdown/manual/admin-guide/01_introduction.md
+++
@@ -1,3 +0,0 @@
-# Introduction
-
-blah
