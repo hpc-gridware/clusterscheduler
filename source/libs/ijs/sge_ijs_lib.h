e77bda5980b45b4d234a4dd84bbe0bec278933cb
Joachim Gabler <jgabler@hpc-gridware.com>
2024-01-29 13:39:08 +0100

    TA: OGE-97 build libijs as C++ code

--- source/libs/ijs/sge_ijs_lib.h
+++ source/libs/ijs/sge_ijs_lib.h
@@ -1,13 +1,7 @@
 #pragma once
+/*___INFO__MARK_BEGIN__*/
 /* Portions of this code are Copyright 2011 Univa Inc. */
+/*___INFO__MARK_END__*/
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-int continue_handler (COMM_HANDLE *comm_handle, char *hostname); 
+int continue_handler (COMM_HANDLE *comm_handle, char *hostname);
 int suspend_handler (COMM_HANDLE *comm_handle, char *hostname, int b_is_rsh, int b_suspend_remote, unsigned int pid, dstring *dbuf);
-
-#ifdef __cplusplus
-}
-#endif
