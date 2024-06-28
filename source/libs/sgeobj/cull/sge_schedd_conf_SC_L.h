a9047a884224dfaf3acd2e9dcb0ac5ee32a40474
Joachim Gabler <jgabler@hpc-gridware.com>
2024-06-28 17:47:23 +0200

    EH: CS-40 add documentation comments from old CULL files to the JSON files // SC_Type

--- source/libs/sgeobj/cull/sge_schedd_conf_SC_L.h
+++ source/libs/sgeobj/cull/sge_schedd_conf_SC_L.h
@@ -28,117 +28,124 @@
 #include "sgeobj/cull/sge_boundaries.h"
 
 /**
-* @brief @todo add summary
+* @brief Object containing scheduler configuration attributes
 *
-* @todo add description
+* In the corresponding master list there is exactly one element holding the scheduler configuration.
+* See also man page sge_sched_conf(5).
 *
-*    SGE_STRING(SC_algorithm) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_algorithm) - Scheduler Algorithm
+*    Deprecated and may be removed in the future.
+*    Was intended to differentiate settings for different scheduling algorithms.
+*    There is exactly one algorithm called default.
 *
-*    SGE_STRING(SC_schedule_interval) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_schedule_interval) - Scheduling Interval
+*    Fixed interval (time value) defining how often scheduling runs are done.
 *
-*    SGE_ULONG(SC_maxujobs) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_maxujobs) - Maximum User Jobs
+*    The maximum number of *running* jobs per user.
 *
-*    SGE_ULONG(SC_queue_sort_method) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_queue_sort_method) - Queue Sort Method
+*    For scheduling jobs the queue instances the sort order can be specified as
+*      - seqno: requires that sequence numbers are defined in the queue definitions
+*      - load: sorting is done by load, @see also load_formula
 *
-*    SGE_LIST(SC_job_load_adjustments) - @todo add summary
-*    @todo add description
+*    SGE_LIST(SC_job_load_adjustments) - Job Load Adjustments
+*    Adds virtual load to just started jobs.
 *
-*    SGE_STRING(SC_load_adjustment_decay_time) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_load_adjustment_decay_time) - Load Adjustment Decay Time
+*    How fast to decay virtual load added via @see job_load_adjustments.
 *
-*    SGE_STRING(SC_load_formula) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_load_formula) - Load Formula
+*    Algebraic expression based on load values and weighting factors.
 *
-*    SGE_STRING(SC_schedd_job_info) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_schedd_job_info) - Schedd Job Info
+*    Boolean which defines if information why jobs cannot be schedules shall be made available.
 *
-*    SGE_ULONG(SC_flush_submit_sec) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_flush_submit_sec) - Flush Submit Sec
+*    Forces scheduling run n seconds after job submission.
 *
-*    SGE_ULONG(SC_flush_finish_sec) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_flush_finish_sec) - Flush Finish Sec
+*    Forces a scheduling run n seconds after a job finished.
 *
-*    SGE_STRING(SC_params) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_params) - Params
+*    Arbitrary list of scheduler parameters, usually name-value pairs.
 *
-*    SGE_STRING(SC_reprioritize_interval) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_reprioritize_interval) - Reprioritize Interval
+*    Time value defining how often running jobs will be reprioritized (based on policies).
 *
-*    SGE_ULONG(SC_halftime) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_halftime) - Halftime
+*    Halftime for decaying sharetree usage.
 *
-*    SGE_LIST(SC_usage_weight_list) - @todo add summary
-*    @todo add description
+*    SGE_LIST(SC_usage_weight_list) - Usage Weight List
+*    Defines how different usage values (e.g. cpu, mem, io) are weighted in sharetree calculations.
 *
-*    SGE_DOUBLE(SC_compensation_factor) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_compensation_factor) - Compensation Factor
+*    Defines how fast too low or too high sharetree usage is compensated.
 *
-*    SGE_DOUBLE(SC_weight_user) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_weight_user) - Weight User
+*    Weighting factor for functional policy on user configuration.
 *
-*    SGE_DOUBLE(SC_weight_project) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_weight_project) - Weight Project
+*    Weighting factor for functional policy on project configuration.
 *
-*    SGE_DOUBLE(SC_weight_department) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_weight_department) - Weight Department
+*    Weighting factor for functional policy on department configuration.
 *
-*    SGE_DOUBLE(SC_weight_job) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_weight_job) - Weight Job
+*    Weighting factor for functional policy on job shares (qsub -js).
 *
-*    SGE_ULONG(SC_weight_tickets_functional) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_weight_tickets_functional) - Weight Tickets Functional
+*    Weight of functional policy as compared to other ticket policies.
 *
-*    SGE_ULONG(SC_weight_tickets_share) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_weight_tickets_share) - Weight Tickets Share
+*    Weight of sharetree policy as compared to other ticket policies.
 *
-*    SGE_ULONG(SC_weight_tickets_override) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_weight_tickets_override) - Weight Tickets Override
+*    Weight of override policy as compared to other ticket policies.
 *
-*    SGE_BOOL(SC_share_override_tickets) - @todo add summary
-*    @todo add description
+*    SGE_BOOL(SC_share_override_tickets) - Share Override Tickets
+*    Boolean defining if every job gets the same amount of tickets from a sharetree node
+*    or if tickets shall be distributed over the jobs belonging to a sharetree node.
 *
-*    SGE_BOOL(SC_share_functional_shares) - @todo add summary
-*    @todo add description
+*    SGE_BOOL(SC_share_functional_shares) - Share Functional Shares
+*    Boolean defining if functional shares of any object instance are shared among
+*    all the jobs associated with the object
 *
-*    SGE_ULONG(SC_max_functional_jobs_to_schedule) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_max_functional_jobs_to_schedule) - Max Functional Jobs to Schedule
+*    The maximum number of functional pending jobs to schedule using the brute-force method.
 *
-*    SGE_BOOL(SC_report_pjob_tickets) - @todo add summary
-*    @todo add description
+*    SGE_BOOL(SC_report_pjob_tickets) - Report Pending Job Tickets
+*    Report pending job tickets to the qmaster via scheduler order or not.
 *
-*    SGE_ULONG(SC_max_pending_tasks_per_job) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_max_pending_tasks_per_job) - Maximum Pending Tasks Per Job
+*    The number of array tasks per pending job to schedule. Helps to reduce scheduling overhead.
 *
-*    SGE_STRING(SC_halflife_decay_list) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_halflife_decay_list) - Halflife Decay List
+*    A list of halflife decay values (UA_Type).
 *
-*    SGE_STRING(SC_policy_hierarchy) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_policy_hierarchy) - Policy Hierarchy
+*    Defines the Order of the ticket computation.
 *
-*    SGE_DOUBLE(SC_weight_ticket) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_weight_ticket) - Weight Ticket
+*    Weight of the ticket policies (functional, override, sharetree) as compared to other policies.
 *
-*    SGE_DOUBLE(SC_weight_waiting_time) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_weight_waiting_time) - Weight Waiting Time
+*    Weight of the waiting time policy as compared to other policies.
 *
-*    SGE_DOUBLE(SC_weight_deadline) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_weight_deadline) - Weight Deadline
+*    Weight of the deadline policy as compared to other policies.
 *
-*    SGE_DOUBLE(SC_weight_urgency) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_weight_urgency) - Weight Urgency
+*    Weight of the urgency policy as compared to other policies.
 *
-*    SGE_DOUBLE(SC_weight_priority) - @todo add summary
-*    @todo add description
+*    SGE_DOUBLE(SC_weight_priority) - Weight Priority
+*    Weight of the posix priority as compared to other policies.
 *
-*    SGE_ULONG(SC_max_reservation) - @todo add summary
-*    @todo add description
+*    SGE_ULONG(SC_max_reservation) - Maximum Number of Reservations
+*    The maximum number of resource reservations. 0 disables resource reservation.
 *
-*    SGE_STRING(SC_default_duration) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SC_default_duration) - Default Duration
+*    Default duration assumed for jobs that specify no h_rt/s_rt.
 *
 */
 
