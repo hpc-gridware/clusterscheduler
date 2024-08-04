92d1b86a73c69c205e1e09a69df19490bc286a72
Joachim Gabler <jgabler@hpc-gridware.com>
2024-08-04 13:01:55 +0200

    EH: CS-40 add documentation comments from old CULL files to the JSON files // SPR_Type

--- source/libs/sgeobj/cull/sge_spooling_SPR_L.h
+++ source/libs/sgeobj/cull/sge_spooling_SPR_L.h
@@ -28,59 +28,82 @@
 #include "sgeobj/cull/sge_boundaries.h"
 
 /**
-* @brief @todo add summary
+* @brief Spooling Rule
 *
-* @todo add description
+* A spooling rule describes a certain way to store and retrieve
+* data from a defined storage facility.
+* 
+* Spooling rules can implement spooling to files in a certain
+* directory or spooling into a database, to an LDAP repository, etc.
+* 
+* A spooling context can contain multiple spooling rules.
 *
-*    SGE_STRING(SPR_name) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SPR_name) - Name
+*    Unique name of the rule.
 *
-*    SGE_STRING(SPR_url) - @todo add summary
-*    @todo add description
+*    SGE_STRING(SPR_url) - URL
+*    An url, e.g. a spool directory, a database url etc.
 *
-*    SGE_REF(SPR_option_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_option_func) - Option Function
+*    Function pointer to a function to set any database specific options,
+*    e.g. whether to do database recovery at startup or not.
 *
-*    SGE_REF(SPR_startup_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_startup_func) - Startup Function
+*    Function pointer to a startup function,
+*    e.g. establishing a connection to a database.
 *
-*    SGE_REF(SPR_shutdown_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_shutdown_func) - Shutdown Function
+*    Function pointer to a shutdown function,
+*    e.g. disconnecting from a database or closing file handles.
 *
-*    SGE_REF(SPR_maintenance_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_maintenance_func) - Maintenance Function
+*    Function pointer to a maintenance function for
+*    - creating the database tables / directories in case of filebased spooling
+*    - switching between spooling with/without history
+*    - backup
+*    - cleaning up / compressing database
+*    - etc.
 *
-*    SGE_REF(SPR_trigger_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_trigger_func) - Trigger Function
+*    Function pointer to a trigger function.
+*    A trigger function is used to trigger regular actions, e.g.
+*    checkpointing and cleaning the transaction log in case of the
+*    Berkeley DB or vacuuming in case of PostgreSQL.
 *
-*    SGE_REF(SPR_transaction_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_transaction_func) - Transaction Function
+*    Function pointer to a function beginning and ending transactions.
+*    
 *
-*    SGE_REF(SPR_list_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_list_func) - List Function
+*    Pointer to a function reading complete lists (master lists)
+*    from the spooling data source.
 *
-*    SGE_REF(SPR_read_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_read_func) - Read Function
+*    Pointer to a function reading a single object from the
+*    spooling data source.
 *
-*    SGE_REF(SPR_read_keys_func) - read keys from the database
-*    reads all keys from a spooling database
-*    matching beginning with a certain pattern
+*    SGE_REF(SPR_read_keys_func) - Read Keys Function
+*    Reads all keys from a spooling database
+*    matching beginning with a certain pattern.
 *    @see e.g. spool_berkeleydb_read_keys()
 *
-*    SGE_REF(SPR_write_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_write_func) - Write Function
+*    Pointer to a function writing a single object.
 *
-*    SGE_REF(SPR_delete_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_delete_func) - Delete Function
+*    Pointer to a function deleting a single object.
+*    
 *
-*    SGE_REF(SPR_validate_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_validate_func) - Validate Function
+*    Pointer to a function validating a single object.
 *
-*    SGE_REF(SPR_validate_list_func) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_validate_list_func) - Validate List Function
+*    Pointer to a function validating a list of objects.
+*    
 *
-*    SGE_REF(SPR_clientdata) - @todo add summary
-*    @todo add description
+*    SGE_REF(SPR_clientdata) - Client Data
+*    Clientdata; any pointer, can be used to store and
+*    reference rule specific data, e.g. file or database handles.
 *
 */
 
