14611e77e2a543a2427c99dcd8cbddcef11cceac
Joachim Gabler <jgabler@hpc-gridware.com>
2024-09-11 09:00:46 +0200

    updated version in inst_sge

--- source/libs/gdi/version.cc
+++ source/libs/gdi/version.cc
@@ -39,6 +39,7 @@
 const char GDI_VERSION[] = "9.0.0 RC1";
 
 // TODO: Add also an entry to the table further down below when you change this
+//       And change VERSION in dist/inst_sge
 const gdi_ulong32 GRM_GDI_VERSION = 0x10009000;
 
 vdict_t GRM_GDI_VERSION_ARRAY[] = 
