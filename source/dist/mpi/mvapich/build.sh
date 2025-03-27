8fd316a04eec66d7d1c999eece19ea6b3addcaa9
Joachim Gabler <jgabler@hpc-gridware.com>
2025-03-27 13:30:36 +0100

    CS-341 out of the box mpi integrations and examples which can also be used for testing // added a MPI test program // templates for MPICH, MVAPICH, Intel MPI, Open MPI

---
+++ source/dist/mpi/mvapich/build.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#___INFO__MARK_BEGIN_NEW__
+###########################################################################
+#
+#  Copyright 2025 HPC-Gridware GmbH
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+###########################################################################
+#___INFO__MARK_END_NEW__
+
+# This script can be used to build MVAPICH-2, e.g version 4.0
+# usage: build.sh <version> <install_dir>
+
+if [ $# -ne 2 ]; then
+    echo "Usage: `basename $0` <version> <install_dir>"
+    exit 1
+fi
+
+CONFIGURE_OPTIONS=""
+# if Fortran is not required
+#CONFIGURE_OPTIONS="$CONFIGURE_OPTIONS --disable-fortran --disable-f77 --disable-f90 --disable-f08"
+# just for testing without having a high speed network
+CONFIGURE_OPTIONS="$CONFIGURE_OPTIONS --with-device=ch3"
+
+VERSION=$1
+INSTALL_DIR=$2
+
+BUILD_DIR=$(pwd)
+SOURCE_TGZ="https://mvapich.cse.ohio-state.edu/download/mvapich/mv2/mvapich-$VERSION.tar.gz"
+
+# download the source
+wget $SOURCE_TGZ
+tar -xzf mvapich-$VERSION.tar.gz
+cd mvapich-$VERSION
+
+# configure and build
+./configure $CONFIGURE_OPTIONS --prefix=$INSTALL_DIR
+make -j
+make install
+
+# cleanup
+cd $BUILD_DIR
+rm -rf mvapich-$VERSION
+rm -f mvapich-$VERSION.tar.gz
+
+exit 0
