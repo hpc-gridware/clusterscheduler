26760986266222b35bf4dcde0b7c0f1c8a3f500d
Joachim Gabler <jgabler@hpc-gridware.com>
2025-04-25 16:06:56 +0200

    EH: CS-1192 at startup of daemons output the cgroups slice the service is running in

--- source/daemons/common/shutdown.cc
+++ source/daemons/common/shutdown.cc
@@ -32,6 +32,9 @@
  ************************************************************************/
 /*___INFO__MARK_END__*/
 #include <cstdio>
+#include <string>
+#include <filesystem>
+#include <fstream>
 
 #include "uti/sge_arch.h"
 #include "uti/sge_log.h"
@@ -45,32 +48,47 @@
 
 void starting_up()
 {
-   u_long32 old_ll = log_state_get_log_level();
-   dstring ds;
-   dstring ds2 = DSTRING_INIT;
-   dstring ds3 = DSTRING_INIT;
-   char buffer[256];
-
    DENTER(TOP_LAYER);
 
-   sge_dstring_init(&ds, buffer, sizeof(buffer));
+   // switch to the INFO logging level, we want to see the startup messages regardless of the configured log level
+   u_long32 old_ll = log_state_get_log_level();
    log_state_set_log_level(LOG_INFO);
    
+   DSTRING_STATIC(ds, 256);
+   dstring ds2 = DSTRING_INIT;
+   dstring ds3 = DSTRING_INIT;
+
    if (feature_get_active_featureset_id() == FEATURE_NO_SECURITY) {
       sge_dstring_copy_string(&ds2, feature_get_product_name(FS_VERSION, &ds)); 
    } else {   
-      sge_dstring_sprintf(&ds2, "%s (%s)", 
-                          feature_get_product_name(FS_VERSION, &ds),
-                          feature_get_featureset_name(
-                                       feature_get_active_featureset_id())); 
+      sge_dstring_sprintf(&ds2, "%s (%s)", feature_get_product_name(FS_VERSION, &ds),
+                          feature_get_featureset_name( feature_get_active_featureset_id()));
    }
    INFO(MSG_STARTUP_STARTINGUP_SSS, feature_get_product_name(FS_SHORT, &ds3), sge_dstring_get_string(&ds2), sge_get_arch());
+   sge_dstring_free(&ds2);
+   sge_dstring_free(&ds3);
+
+
+   // log if we are using Munge
    if (bootstrap_get_use_munge()) {
       INFO(SFNMAX, MSG_STARTUP_USING_MUNGE);
    }
 
-   sge_dstring_free(&ds2);
-   sge_dstring_free(&ds3);
+   // if we are running within a cgroup then output the cgroup path
+#if defined (LINUX)
+   std::string proc_cgroup = "/proc/self/cgroup";
+   if (std::filesystem::exists(proc_cgroup)) {
+      std::ifstream cgroup_file(proc_cgroup);
+      if (cgroup_file.is_open()) {
+         std::string cgroup;
+         if (std::getline(cgroup_file, cgroup)) {
+            INFO(MSG_STARTUP_IN_CGROUP_S, cgroup.c_str());
+         }
+      }
+   }
+#endif
+
+   // reset the log level to the previous value
    log_state_set_log_level(old_ll);
 
    DRETURN_VOID;
