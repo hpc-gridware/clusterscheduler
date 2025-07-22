8080ed8d7169c4b526687612689acf8f07e916e4
Joachim Gabler <jgabler@hpc-gridware.com>
2025-07-22 20:50:33 +0200

    added missing files

---
+++ source/daemons/common/ocs_common_systemd.h
@@ -0,0 +1,27 @@
+#pragma once
+/*___INFO__MARK_BEGIN_NEW__*/
+/***************************************************************************
+ *
+ *  Copyright 2025 HPC-Gridware GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ***************************************************************************/
+/*___INFO__MARK_END_NEW__*/
+
+#include "sgeobj/sge_conf.h"
+
+namespace ocs::common {
+   bool
+   use_pdc_for_usage_collection(usage_collection_t usage_collection);
+}
