308de81c0b9b015b6d8e4e1bcdd96b1e9cf9fd0e
Joachim Gabler <jgabler@hpc-gridware.com>
2026-01-27 12:40:21 +0100

    BF: CS-1760 with reporting disabled there is increased spooling activity at midnight for 10 minutes

--- source/daemons/qmaster/ocs_JsonReportingFileWriter.cc
+++ source/daemons/qmaster/ocs_JsonReportingFileWriter.cc
@@ -1,20 +1,20 @@
 /*___INFO__MARK_BEGIN_NEW__*/
 /***************************************************************************
- *  
- *  Copyright 2024-2025 HPC-Gridware GmbH
- *  
+ *
+ *  Copyright 2024-2026 HPC-Gridware GmbH
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  
+ *
  ***************************************************************************/
 /*___INFO__MARK_END_NEW__*/
 
@@ -62,7 +62,7 @@ namespace ocs {
       // we need to protect the usage_pattern_list via the config_mutex
       sge_mutex_lock(config_mutex_name.c_str(), __func__, __LINE__, &config_mutex);
       bool ret = sge_write_rusage(nullptr, &writer, job_report, job, ja_task, category_string, &usage_pattern_list, 0,
-                             false, true);
+                             intermediate, true);
       sge_mutex_unlock(config_mutex_name.c_str(), __func__, __LINE__, &config_mutex);
 
       if (ret) {
