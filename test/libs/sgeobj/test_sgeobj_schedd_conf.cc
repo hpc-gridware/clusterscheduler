adeeb6f38dcab30f7c1294e159a45377867a05f6
Ernst Bablick <ebablick@hpc-gridware.com>
2026-05-09 23:20:56 +0200

    BF: updated date in copyright comments

--- test/libs/sgeobj/test_sgeobj_schedd_conf.cc
+++ test/libs/sgeobj/test_sgeobj_schedd_conf.cc
@@ -27,7 +27,7 @@
  *
  *   All Rights Reserved.
  *
- *  Portions of this software are Copyright (c) 2023-2024 HPC-Gridware GmbH
+ *  Portions of this software are Copyright (c) 2023-2026 HPC-Gridware GmbH
  *
  ************************************************************************/
 /*___INFO__MARK_END__*/
