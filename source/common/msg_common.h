d5dea72a39e95bc3e1a357191a7866b984774dc2
Ernst Bablick <ebablick@hpc-gridware.com>
2026-05-13 01:03:01 +0200

    EH: CS-2160: Document qrsh/qlogin `-suspend_remote` option in man pages

--- source/common/msg_common.h
+++ source/common/msg_common.h
@@ -923,9 +923,7 @@
 #define MSG_GDI_UTEXT_sst_OPT                            _MESSAGE(23514, _("show a formated sharetree"))
 
 #define MSG_GDI_USAGE_suspend_remote_OPT                 "[-suspend_remote y[es]|n[o]]"
-#define MSG_GDI_UTEXT_suspend_remote_OPT                 _MESSAGE(23515, _("suspend qrsh client as also the process on execution host"))
-
-#define MSG_PARSE_FEATURENOTSUPPORTEDOS_S                _MESSAGE(23516, _("Unsupported: " SFQ " is not supported in the open source version"))
+#define MSG_GDI_UTEXT_suspend_remote_OPT                 _MESSAGE(23515, _("also suspend whole remote job tree on Ctrl+Z (extra SIGSTOP)"))
 
 #define MSG_GDI_USAGE_scope_OPT                          "[-scope scope_name]"
 #define MSG_GDI_UTEXT_scope_OPT                          _MESSAGE(23517, _("switch the request scope"))
